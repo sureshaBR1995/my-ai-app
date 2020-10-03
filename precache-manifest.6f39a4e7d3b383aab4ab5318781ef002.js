29f75490330750ed6f872d085a623f1cd48ed9f0
sureshaBR1995 <sureshblk95@gmail.com>
2020-10-03 19:04:01 +0530

    Updates

---
+++ precache-manifest.6f39a4e7d3b383aab4ab5318781ef002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a8593919230ce8557acbf2cdf32884",
+    "url": "/my-ai-app/index.html"
+  },
+  {
+    "revision": "a14b4225ff1b2b73d631",
+    "url": "/my-ai-app/static/css/main.19434af2.chunk.css"
+  },
+  {
+    "revision": "4a1115b36326b3b09717",
+    "url": "/my-ai-app/static/js/2.03ce6e9d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/my-ai-app/static/js/2.03ce6e9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a14b4225ff1b2b73d631",
+    "url": "/my-ai-app/static/js/main.82b79a42.chunk.js"
+  },
+  {
+    "revision": "4a91f80c511c12640b41",
+    "url": "/my-ai-app/static/js/runtime-main.6909950d.js"
+  }
+]);
